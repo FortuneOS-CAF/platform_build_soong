c49f9d82a6950ad21c8810d2dda6a32bb28e5886
Jenna-they-them <jenna@jenslody.de>
2024-03-07 10:26:42 +0100

    soong: installclean: remove changelog help-files
    manifest.xml{,.tmp} are used to check for repo changes after creating
    the last changelog
    
    On some system they lead to errors, if they remain, and Changelog.txt is removed.
    
    Signed-off-by: Jenna-they-them <jenna@jenslody.de>

--- ui/build/cleanbuild.go
+++ ui/build/cleanbuild.go
@@ -153,6 +153,7 @@ func installClean(ctx Context, config Config) {
 		hostOut("vts-core"),
 		hostCommonOut("obj/PACKAGING"),
 		productOut("*Changelog.txt"),
+		productOut("manifest.xml*"),
 		productOut("*.img"),
 		productOut("*.zip"),
 		productOut("android-info.txt"),
