LineageOS/android_build_soong

Add vendor/lineage-priv/keys to allowlist
Product dev certificate now requires android_certificate_directory file
group.

Change-Id: Ic12e41ca8d59ca4fea4238b57cf2feaf53b0532d

android/allowlists/allowlists.go
@@ -440,6 +440,8 @@ var (
 
 		// TODO(b/266459895): remove this and the placeholder BUILD file after re-enabling libunwindstack
 		"external/rust/crates/rustc-demangle-capi":/* recursive = */ false,
+
+		"vendor/lineage-priv/keys":/* recursive = */ false,
 	}
 
 	Bp2buildModuleAlwaysConvertList = []string{

FortuneOS-CAF/platform_build_soong

android/allowlists/allowlists.go
@@ -440,6 +440,8 @@ var (
 
 		// TODO(b/266459895): remove this and the placeholder BUILD file after re-enabling libunwindstack
 		"external/rust/crates/rustc-demangle-capi":/* recursive = */ false,
+
+		"vendor/fortune-priv/keys":/* recursive = */ false,
 	}
 
 	Bp2buildModuleAlwaysConvertList = []string{